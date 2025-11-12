84b8a37c2d0883065517d1f5e044919f0d80ade1
H1vee <ailedinskyi@gmail.com>
2025-11-12 20:06:06 +0200

    Update

--- app/src/main/java/com/example/reserve/MainActivity.kt
+++ app/src/main/java/com/example/reserve/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.reserve
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupLogo()
+    }
+
+    private fun setupLogo() {
+        val tvLogo = findViewById<TextView>(R.id.tvLogo)
+        val logoText = "Резерв+"
+        val spannable = SpannableString(logoText)
+
+        // Зробити "+" оранжевим
+        val orangeColor = ContextCompat.getColor(this, R.color.orange_bright)
+        spannable.setSpan(
+            ForegroundColorSpan(orangeColor),
+            6, // позиція "+"
+            7, // кінець "+"
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        tvLogo.text = spannable
     }
 }
