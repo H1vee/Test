44c0c6e7decc6a93bf5a82e379f4288b573f99e5
H1vee <ailedinskyi@gmail.com>
2025-11-13 21:47:28 +0200

    Update

--- app/src/main/java/com/example/reserve/MainActivity.kt
+++ app/src/main/java/com/example/reserve/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.reserve
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         setupLogo()
         setupMarquee()
+        setupMenuButton()
     }
 
     private fun setupLogo() {
@@ -24,5 +27,60 @@ class MainActivity : AppCompatActivity() {
     private fun setupMarquee() {
         val tvMarquee = findViewById<TextView>(R.id.tvMarquee)
         tvMarquee.isSelected = true
+    }
+
+    private fun setupMenuButton() {
+        val btnMenu = findViewById<ImageView>(R.id.btnMenu)
+        btnMenu.setOnClickListener {
+            showBottomSheetMenu()
+        }
+    }
+
+    private fun showBottomSheetMenu() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val view = layoutInflater.inflate(R.layout.bottom_sheet_menu, null)
+
+        view.findViewById<android.view.View>(R.id.btnViewDocument).setOnClickListener {
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        view.findViewById<android.view.View>(R.id.btnDownloadPDF).setOnClickListener {
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        view.findViewById<android.view.View>(R.id.btnRefreshDocument).setOnClickListener {
+            // Оновити документ
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        view.findViewById<android.view.View>(R.id.btnExtendedData).setOnClickListener {
+            // Розширені дані з реєстру
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        view.findViewById<android.view.View>(R.id.btnRequestDeferment).setOnClickListener {
+            // Подати запит на відстрочку
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        view.findViewById<android.view.View>(R.id.btnMedicalDirection).setOnClickListener {
+            // Направлення на ВЛК
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        view.findViewById<android.view.View>(R.id.btnUpdateContacts).setOnClickListener {
+            // Уточнити контактні дані
+            bottomSheetDialog.dismiss()
+            // TODO: Добавить функционал
+        }
+
+        bottomSheetDialog.setContentView(view)
+        bottomSheetDialog.show()
     }
 }
